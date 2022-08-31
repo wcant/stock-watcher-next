ce21b6962df19751ff1687698c198d84f3dc7701
Wes Cantrell <wes@init1.io>
2022-08-31 18:43:32 -0500

    deploy to heroku

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./styles/App.css";
+
+function App() {
+  const url = "http://localhost:4000/api";
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    try {
+      fetch(url, {
+        json: true,
+        headers: { "User-Agent": "request" },
+      })
+        .then((res) => {
+          if (res.status !== 200) {
+            console.log("Status:", res.status);
+          } else {
+            // data is successfully parsed as a JSON object:
+            return res.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          setData(data);
+        });
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  });
+
+  return (
+    <div className="App">
+      <p>Data Output:</p>
+      <p>{!data ? "Loading..." : data}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=${REACT_APP_API_KEY}`;
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "hello from server" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`proxy listening on ${PORT}`);
+});
