00507e16fde391efe86d18f79c31fb776a8243f5
Wes Cantrell <wes@init1.io>
2022-08-31 19:46:15 -0500

    debugging cors

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./styles/App.css";
 
 function App() {
-  const url = "http://localhost:4000/api";
+  const url = "https://stock-watcher-wcant.herokuapp.com/api";
 
   const [data, setData] = useState(null);
 
