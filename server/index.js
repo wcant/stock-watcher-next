1f38e343a9669e5d7fd004698f508f99e08a9b39
Wes Cantrell <wes@init1.io>
2022-08-31 19:42:23 -0500

    debugging cors

--- server/index.js
+++ server/index.js
@@ -7,14 +7,21 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 // const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=${REACT_APP_API_KEY}`;
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(cors());
 
-app.disable("x-powered-by");
-
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.get("/api", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   res.json({ message: "hello from server" });
 });
 
